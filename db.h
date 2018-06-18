bc1221e6cc27e0f504c2839493bbd443d58ee46a
cronos <cronos@polispay.org>
2018-06-18 13:31:21 -0500

    Bump

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 60000;
+    return testnet ? 0 : 100000;
 }
 
 std::string static inline ToString(const CService &ip) {
